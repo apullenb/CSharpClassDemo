c9f5d4f1b3d5ad0db92e63eb6debe95fb991e0a4
Andrea Pullen <70964079+apullenb@users.noreply.github.com>
2021-09-20 13:02:26 -0500

    Create class w/ inheritance

---
+++ CSharpClasses/CSharpClasses/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClasses
+{
+    class Post
+    {
+        private static int currentPostId;
+
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        protected string SentByUsername { get; set; }
+        
+        protected bool IsPublic { get; set; }
+
+        public Post()
+        {
+            ID = 0;
+            Title = "My First Post";
+            IsPublic = true;
+            SentByUsername = "Default User";
+        }
+
+        public Post(string title, bool isPublic, string sentByUsername)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SentByUsername = sentByUsername;
+            this.IsPublic = isPublic;
+        }
+        protected int GetNextID()
+        {
+            return ++currentPostId;
+        }
+
+        public void Update(string title, bool isPublic)
+        {
+            this.Title = title;
+            this.IsPublic = isPublic;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - by {2}", this.ID, this.Title, this.SentByUsername);
+        }
+    }
+}
--- CSharpClasses/CSharpClasses/Program.cs
+++ CSharpClasses/CSharpClasses/Program.cs
@@ -6,7 +6,16 @@ namespace CSharpClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Post post1 = new Post("Thank You!", true, "Andrea");
+
+            Post post2 = new Post("Second Post", true, "Andrea P");
+            Console.WriteLine(post1.ToString());
+            Console.WriteLine(post2.ToString());
+           
+
+            ImagePost imagePost1 = new ImagePost("New Shoes!", "Andrea P", "http://www.facebook.com", true);
+            Console.WriteLine(imagePost1.ToString());
+            Console.ReadLine();
         }
     }
 }
